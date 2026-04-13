710eaea07f4d67901b13019a3cd414cc5856fae2
Yuricwx <isabellegonzales606@gmail.com>
2026-04-13 17:18:43 -0300

    função do front arrumada(sobre nos)

--- BackEnd/src/routes/Routes.ts
+++ BackEnd/src/routes/Routes.ts
@@ -20,7 +20,7 @@ import {
   deletarPedido 
 } from '../controllers/pedidosController.js';
 import { login, registrar } from '../controllers/authcontroller.js';
-import { registraAuthmeio } from '../middleware/auth.js';
+import { Authmeio } from '../middleware/auth.js';
 
 const routes = Router();
 
@@ -29,22 +29,22 @@ routes.post('/login', login)
 routes.post('/registrar', registrar)
 
 // --- ROTAS DE USUÁRIOS ---
-routes.get('/usuarios', registraAuthmeio, listarUsuarios);
+routes.get('/usuarios', Authmeio, listarUsuarios);
 routes.get('/usuarios/:id', buscarUsuario);
-routes.put('/usuarios/:id', atualizarUsuario);
+routes.put('/usuarios/:id', Authmeio, atualizarUsuario);
 routes.delete('/usuarios/:id', deletarUsuario);
 
 // --- ROTAS DE GATOS ---
 routes.get('/gatos', listarGatos);
-routes.post('/gatos', criarGato);
+routes.post('/gatos', Authmeio, criarGato);
 routes.get('/gatos/:id', buscarGato);
 routes.put('/gatos/:id', atualizarGato);
 routes.delete('/gatos/:id', deletarGato);
 
 // --- ROTAS DE PEDIDOS DE ADOÇÃO ---
-routes.get('/pedidos', listarPedidos);
-routes.post('/pedidos', criarPedido);
-routes.patch('/pedidos/:id', atualizarPedido);
+routes.get('/pedidos', Authmeio, listarPedidos);
+routes.post('/pedidos', Authmeio, criarPedido);
+routes.patch('/pedidos/:id', Authmeio, atualizarPedido);
 routes.delete('/pedidos/:id', deletarPedido);
 
 export default routes;
