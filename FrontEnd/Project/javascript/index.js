7bd1b5ce6fed65f8c2cebbd478547648d95986ea
Yuricwx <isabellegonzales606@gmail.com>
2026-04-15 16:40:45 -0300

    tentativa de integração

--- FrontEnd/Project/javascript/index.js
+++ FrontEnd/Project/javascript/index.js
@@ -8,7 +8,12 @@ const fecharPop = document.querySelector("dialog button")
 function abrirModal(titulo, desc,img){
     document.querySelector(".modal-img").src = img
     document.querySelector(".modal-titulo").innerHTML = titulo
-    document.querySelector(".desc").innerHTML = desc
+    document.querySelector(".desc").innerHTML = descricao
+    document.querySelector("idade").innerHTML = idade
+    document.querySelector("sexo").innerHTML = sexo
+    document.querySelector("vacinado").innerHTML = vacinado
+    document.querySelector("castrado").innerHTML = castrado
+    document.querySelector("raca").innerHTML = raca
 
     modal.showModal()
 }
@@ -20,9 +25,34 @@ fecharPop.onclick = function(){
 async function carregarGatos() {
     const response = await fetch('http://localhost:3000/gatos')
     const data = await response.json()
-    const select = document.getElementById("mostrarGatos")
-    select.innerHTML = data.map(g => `option value =${g.id} >${g.nome, g.raca, g.idade, g.descricao, g.sexo,g.porte, g.foto_principal,g.cadastrado,g.vacinado, g.castrado,g.id_usuario}`
-    ).join("")
+    const select = document.getElementById("cards")
+    console.log(select);
+    
+    // select.innerHTML = data.map(g => `<h2> =${g.nome}</h2> <dialog><p class="raca">=${g.raca}</p></dialog>,
+    //  <h4>=${g.idade}</h4> <dialog><p>=${g.descricao}</p></dialog>, <dialog><p class="sexo">=${g.sexo}</p></dialog>,
+    //  <dialog><img class="modal-img"=${g.foto_principal}>,<dialog><p class="vacinado">=${g.vacinado}</p></dialog>, <dialog><p class="castrado">= ${g.castrado}</p></dialog>`
+    // ).join("")
+    select.innerHTML = data.map(g => `
+                        <div class="card">
+                    <img src="${g.foto_principal}" alt="gato filhote tricolor">
+                    <h2>${g.nome}</h2>
+                    <h4>${g.idade}</h4>
+                    <p class="stats"><i class="bi bi-patch-check"></i>${g.vacinado}</p>
+                    <p class="stats"><i class="bi bi-patch-check"></i>${g.castrado}</p>
+                    <a class="buttonA" onclick="abrirModal('${g.foto_principal}','${g.nome}','${g.descricao}','${g.foto_principal}', '${g.idade}', '${g.sexo}', '${g.vacinado}', '${g.castrado}', '${g.raca}')" >Quero Adotar</a>
+                    <dialog>
+                         <img src="${g.foto_principal}" alt="" class="modal-img">
+                        <h1 class="modal-titulo">${g.nome}</h1>
+                        <p class="desc">${g.descricao}</p>
+                        <p class="idade">${g.idade}</p>
+                        <p class="sexo">${g.sexo}</p>
+                        <p class="vacinado">${g.vacinado}</p>
+                        <p class="castrado">${g.castrado}</p>
+                        <p class="raca">${g.raca}</p>
+                        <button>Fechar</button>
+                    </dialog>
+                </div>
+        `)
 
 }
 carregarGatos()
